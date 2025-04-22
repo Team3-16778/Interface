2b57604234eac174c4ff48acee862d4976a64a33
Chase <mcallen2@andrew.cmu.edu>
2025-04-22 16:12:32 -0400

    updates

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import serial
 import time
 
-port = '/dev/ttyACM1'
+port = '/dev/ttyACM0'
 
-ser = serial.Serial(port, 9600, timeout=1)
+ser = serial.Serial(port, 9600,timeout=1)
 
-command = "ROTATE 100 10"
+command = "HOME\n"
 
-while True:
-    ser.write(command.encode())
-    time.sleep(2)
-    response = ser.readline().decode().strip()
-    print(response)
+
+ser.write(command.encode())
+time.sleep(2)
+response = ser.readline().decode().strip()
+print(response)
 
 ser.close()
